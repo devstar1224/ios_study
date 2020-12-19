9b8ed8491686a7e4fca3f67294b6ff80225bcc1b
devstar1224 <dev_sang@naver.com>
2020-12-19 23:23:10 +0900

    ViewController를 이용한 Controller 전환.
    navigationController을 이용하여 pushViewController를 사용하여 스토리보드 컨트롤러 찾기.
    변수의 옵셔널 바인딩 처리.

---
+++ ios_study/DetailController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailController.swift
+//  ios_study
+//
+//  Created by Sangik Lee on 2020/12/19.
+//
+
+import UIKit
+
+class DetailController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- ios_study/ViewController.swift
+++ ios_study/ViewController.swift
@@ -14,8 +14,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func ClickMoveBtn(_ sender: Any) {
-        print("click button")
+    @IBAction func MoveStory(_ sender: Any) {
+        //변수
+        
+        //1. storyboard find controller :
+        
+        //if 안에 변수 넣으면 옵셔널 바인딩.
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DetailController") {
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+
+    
+        
+        //2. move controller.
+      
     }
+    
 }
 
